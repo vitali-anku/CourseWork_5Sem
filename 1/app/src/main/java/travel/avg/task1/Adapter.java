379772128f2fab2e3ba13be4f82d164a0f294f9c
Vitali Ankudovich <vitalja.ja@gmail.com>
2018-09-14 21:46:09 +0300

    fices bages 2.0

--- 1/app/src/main/java/travel/avg/task1/Adapter.java
+++ 1/app/src/main/java/travel/avg/task1/Adapter.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
